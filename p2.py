1f4c593ed8f4b13128f43df9c979f042221221d8
avikejriwal <avi.kejriwal@gmail.com>
2017-03-07 18:21:14 -0600

    first few projects

---
+++ p2.py
@@ -0,0 +1,18 @@
+import numpy as np
+from decimal import Decimal
+
+tot = 0
+
+maxVal = 4*(10**6)
+x = 1
+y = 1
+z = 0
+
+while z < maxVal:
+    z = x+y
+    if z % 2 == 0:
+        tot += z
+    x = y
+    y = z
+
+print tot
